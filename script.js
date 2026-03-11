86a8b9603dfd10696884a7bd9c5f306c0252f77f
pedrolimaoliveira11-maker <pedro.lima.oliveira11@escola.pr.gov.br>
2026-03-11 08:27:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-@keyframes fadeOut {
-    0% { opacity: 1; transform: translate(-50%, -50%) scale(1); }
-    100% { opacity: 0; transform: translate(-50%, -100%) scale(1.5); }
-}v
+/* Chefão Estilizado */
+.boss { 
+    width: 160px; 
+    height: 90px; 
+    background: linear-gradient(180deg, #4b0082, #000); 
+    position: absolute; 
+    border-radius: 20px 20px 80px 80px; 
+    box-shadow: 0 0 50px #ff00ff; 
+    z-index: 5; 
+    border: 2px solid #ff00ff;
+}
+
+/* Olhos do Chefão */
+.boss::before, .boss::after {
+    content: '';
+    position: absolute;
+    width: 25px;
+    height: 8px;
+    background: #ff0000;
+    top: 35px;
+    box-shadow: 0 0 15px #ff0000;
+    border-radius: 50%;
+}
+.boss::before { left: 35px; transform: rotate(-10deg); }
+.boss::after { right: 35px; transform: rotate(10deg); }
